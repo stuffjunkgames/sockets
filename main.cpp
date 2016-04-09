745314509a13b168b0a0c69b3aaf7420300e7a2a
stuffjunkgames <msquared.169@gmail.com>
2016-04-08 23:38:12 -0400

    two-client interaction

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <SFML/Network.hpp>
 #include <cstdlib>
 #include <iostream>
+#include <time.h>
 
 // client = 1, server = 0
 const bool client = 1;
@@ -11,6 +12,7 @@ const int port = 12345;
 
 int main()
 {
+    // client
     if(client)
     {
         std::cout << "Creating socket on port " << port << "...\n";
@@ -23,23 +25,94 @@ int main()
             return 1;
         }
 
-        // socket connected, share some data
-        char data[7] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
-        if(socket.send(data, 7) != sf::Socket::Done)
-        {
-            std::cout << "Error sending data!\n";
-            return 1;
-        }
-
-        std::size_t received;
-
-        if(socket.receive(data, 7, received) != sf::Socket::Done)
+        sf::Uint16 x;
+        std::string s;
+        sf::Packet packet;
+        if(socket.receive(packet) != sf::Socket::Done)
         {
             std::cout << "Error receiving data!\n";
             return 1;
         }
-        std::cout << "Received " << received << " bytes: " << data[0] << data[1] << data[2] << data[3] << data[4] << data[5] << data[6] << std::endl;
+
+        packet >> x;
+
+        if(x == 1)
+        {
+            std::cout << "Server selected you to go first.  Type something:\n";
+
+            while(1)
+            {
+                std::cin >> s;
+
+                packet << s;
+                if(socket.send(packet) != sf::Socket::Done)
+                {
+                    std::cout << "Error sending data!\n";
+                    return 1;
+                }
+
+                if(s == "exit")
+                {
+                    break;
+                }
+
+                if(socket.receive(packet) != sf::Socket::Done)
+                {
+                    std::cout << "Error receiving data!\n";
+                    return 1;
+                }
+
+                packet >> s;
+                std::cout << s << std::endl;
+
+                if(s == "exit")
+                {
+                    break;
+                }
+            }
+
+        }
+        else if(x == 0)
+        {
+            std::cout << "Server selected other client to go first.  Please wait.";
+
+            while(1)
+            {
+                if(socket.receive(packet) != sf::Socket::Done)
+                {
+                    std::cout << "Error receiving data!\n";
+                    return 1;
+                }
+
+                packet >> s;
+                std::cout << s << std::endl;
+
+                if(s == "exit")
+                {
+                    break;
+                }
+
+                std::cin >> s;
+
+                packet << s;
+                if(socket.send(packet) != sf::Socket::Done)
+                {
+                    std::cout << "Error sending data!\n";
+                    return 1;
+                }
+
+                if(s == "exit")
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            std::cout << "Received something other than 0 or 1\n";
+        }
     }
+    // server
     else
     {
         sf::TcpListener listener;
@@ -52,35 +125,143 @@ int main()
         }
 
         // accept a new connection
-        sf::TcpSocket client;
-        if(listener.accept(client) != sf::Socket::Done)
+        sf::TcpSocket client1;
+        sf::TcpSocket client2;
+        std::cout << "Listening for client connections...\n";
+        if(listener.accept(client1) != sf::Socket::Done || listener.accept(client2) != sf::Socket::Done)
         {
             std::cout << "Error accepting client!\n";
             return 1;
         }
 
-        char data[7];
-        std::size_t received;
+        std::cout << "Both clients connected!\n";
 
-        if(client.receive(data, 7, received) != sf::Socket::Done)
+        // pick a random client to go first
+        std::srand(std::time(NULL));
+        int number = rand() % 2;
+        sf::Uint16 x;
+        sf::Packet packet;
+        if(number == 0)
         {
-            std::cout << "Error receiving data!\n";
-            return 1;
+            std::cout << "Chose client 1 to go first\n";
+            x = 1;
+            packet << x;
+            if(client1.send(packet) != sf::Socket::Done)
+            {
+                std::cout << "Error sending packet to client 1\n";
+                return 1;
+            }
+
+            x = 0;
+            packet << x;
+            if(client2.send(packet) != sf::Socket::Done)
+            {
+                std::cout << "Error sending packet to client 2\n";
+                return 1;
+            }
+
+            std::string s;
+            while(1)
+            {
+                if(client1.receive(packet) != sf::Socket::Done)
+                {
+                    std::cout << "Error receiving packet from client 1\n";
+                    return 1;
+                }
+
+                if(client2.send(packet) != sf::Socket::Done)
+                {
+                    std::cout << "Error sending packet to client 2\n";
+                    return 1;
+                }
+
+                packet >> s;
+                std::cout << s << std::endl;
+                if(s == "exit")
+                {
+                    break;
+                }
+
+                if(client2.receive(packet) != sf::Socket::Done)
+                {
+                    std::cout << "Error receiving packet from client 2\n";
+                    return 1;
+                }
+
+                if(client1.send(packet) != sf::Socket::Done)
+                {
+                    std::cout << "Error sending packet to client 1\n";
+                    return 1;
+                }
+
+                packet >> s;
+                std::cout << s << std::endl;
+                if(s == "exit")
+                {
+                    break;
+                }
+            }
         }
-        std::cout << "Received " << received << " bytes: " << data[0] << data[1] << data[2] << data[3] << data[4] << data[5] << data[6] << std::endl;
-
-        data[0] = data[6];
-        data[6] = data[1];
-        data[1] = data[5];
-        data[5] = data[2];
-        data[2] = data[4];
-        data[4] = data[3];
-
-        if(client.send(data, 7) != sf::Socket::Done)
+        else if(number == 1)
         {
-            std::cout << "Error sending data!\n";
-            return 1;
+            std::cout << "Chose client 2 to go first\n";
+            x = 1;
+            packet << x;
+            if(client2.send(packet) != sf::Socket::Done)
+            {
+                std::cout << "Error sending packet to client 2\n";
+            }
+
+            x = 0;
+            packet << x;
+            if(client1.send(packet) != sf::Socket::Done)
+            {
+                std::cout << "Error sending packet to client 1\n";
+            }
+
+            std::string s;
+            while(1)
+            {
+                if(client2.receive(packet) != sf::Socket::Done)
+                {
+                    std::cout << "Error receiving packet from client 2\n";
+                    return 1;
+                }
+
+                if(client1.send(packet) != sf::Socket::Done)
+                {
+                    std::cout << "Error sending packet to client 1\n";
+                    return 1;
+                }
+
+                packet >> s;
+                std::cout << s << std::endl;
+                if(s == "exit")
+                {
+                    break;
+                }
+
+                if(client1.receive(packet) != sf::Socket::Done)
+                {
+                    std::cout << "Error receiving packet from client 1\n";
+                    return 1;
+                }
+
+                if(client2.send(packet) != sf::Socket::Done)
+                {
+                    std::cout << "Error sending packet to client 2\n";
+                    return 1;
+                }
+
+                packet >> s;
+                std::cout << s << std::endl;
+                if(s == "exit")
+                {
+                    break;
+                }
+            }
         }
+
     }
 
 
